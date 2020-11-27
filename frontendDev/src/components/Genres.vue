2bef90bdbca372852f80c3ba7af5c5656edf30bf
Liam Benedetti <libe@it-tf.local>
2020-11-27 16:33:49 +0100

    friti

---
+++ frontendDev/src/components/Genres.vue
@@ -0,0 +1,229 @@
+<template>
+    <Player :showID="showID"/>
+    <div class="genres">
+        <div class="genre" v-for="genre in shows.genres" :key="genre.length">
+            <h2 class="genreName">{{genre.title}}</h2>
+            <p class="swipeRight" @click="swipe">></p>
+            <p class="swipeLeft"  @click="swipe">&lt;</p>
+            <div class="showWrapper">
+            <div class="show" v-for="show in genre" :key="show.length" @click="openShow()" :id=show.SID>
+                <img :src="show.cover" :alt="show.title">
+                <div class="overview">
+                    <p class="showTitle">{{show.title}}</p>
+                    <div class="showTag" v-for="tag in show.genres" :key="tag">
+                        <p>{{tag}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+import { reactive } from 'vue'
+import Player from "./Player"
+export default {
+    name: "Genres",
+    components: {
+        Player
+    },
+
+    setup() {
+        const host = "http://bstoapp.staging.it-tf.ch/api/"
+        const shows = reactive({genres: {}})
+        const showID = reactive({value: "ddd"})
+
+        async function loadShows() {
+            var genres = await fetch(host + "genres")
+            genres = await genres.json()
+
+            for (let i = 0; i < genres.length; i++) {
+                let genre = genres[i]
+
+                let response = await fetch(host + `shows/genre/${genre}`)
+                shows.genres[genre] = await response.json()
+                shows.genres[genre].title = genre
+            }
+        }
+
+        function openShow() {
+            let target = event.target
+
+            while (target.id == "") {
+                target = target.parentElement
+            }
+
+            showID.value = target.id
+        }
+
+        loadShows()
+        return {shows, showID, openShow}
+    },
+
+    methods: {
+        swipe(event) {
+            const showContainer = event.target.parentElement.childNodes[3]
+            const target = event.target
+
+            let scrollValue = 0
+
+            console.log(target.innerHTML);
+
+            if (target.innerHTML == "&lt;") {
+                scrollValue = showContainer.scrollLeft - showContainer.offsetWidth
+            }
+            else {
+                scrollValue = showContainer.scrollLeft + showContainer.offsetWidth
+            }
+
+            const options = {
+                top: 0,
+                left: scrollValue,
+                behavior: 'smooth'
+            }
+
+            showContainer.scroll(options);
+        },
+    },
+}
+</script>
+
+<style scoped>
+    .genres {
+        width: 100%;
+        height: 100vh;
+        padding-top: 20px;
+    }
+
+    h2 {
+        color: #f1f6f9;
+        padding-top: 5px;
+        padding-left: 5px;
+        position: relative;
+    }
+
+    .genre {
+        position: relative;
+    }
+
+    .swipeRight {
+        position: absolute;
+        right: 0px;
+        text-align: center;
+        font-size: 20pt;
+        color: white;
+        cursor: pointer;
+        background-color: #394867;
+        width: 30px;
+        height: 28vh;
+        margin-top: 17px;
+        opacity: .7;
+        z-index: 1;
+        line-height: 28vh;
+        vertical-align: middle;
+    }
+
+    .swipeRight:hover {
+        opacity: .9;
+        width: 40px;
+    }
+
+    .swipeLeft {
+        position: absolute;
+        left: 0px;
+        text-align: center;
+        font-size: 20pt;
+        color: white;
+        cursor: pointer;
+        background-color: #394867;
+        width: 30px;
+        height: 28vh;
+        margin-top: 17px;
+        opacity: .7;
+        z-index: 1;
+        line-height: 28vh;
+        vertical-align: middle;
+    }
+
+    .swipeLeft:hover {
+        opacity: .9;
+        width: 40px;
+    }
+
+    .showWrapper {
+        width: 100%;
+        overflow-x: scroll;
+        display: flex;
+        padding: 10px;
+        padding-top: 17px;
+        padding-bottom: 17px;
+        position: relative;
+    }
+
+    .showWrapper::-webkit-scrollbar {
+        display: none;
+    }
+
+    .show {
+        height: 28vh;
+        border-radius: 5px;
+        margin-left: 6px;
+        margin-right: 6px;
+        box-shadow: 0px 3px 10px #14274e;
+        cursor: pointer;
+        position: relative;
+    }
+
+    .overview {
+        display: inline-block;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        background-color: #394867;
+        color: white;
+        padding: 5px;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        border-bottom-right-radius: 5px;
+        border-bottom-left-radius: 5px;
+        opacity: 0;
+    }
+
+    .showTitle {
+        color: white;
+        font-size: large;
+        padding: 3px;
+        margin-bottom: 20px;
+    }
+
+    .showTag {
+        background-color: white;
+        border-radius: 12px;
+        color: #394867;
+        display: inline-block;
+        margin: 3px;
+        font-size: small;
+        font-weight: bold;
+        padding: 3px;
+        padding-left: 6px;
+        padding-right: 6px;
+    }
+
+    .show:hover {
+        transform: scale(1.06);
+        position: absolutes;
+        box-shadow: 0px 0px 10px #10203f;
+    }
+
+    .show:hover .overview {
+        opacity: 1;
+    }
+
+    img {
+        display: inline-block;
+        height: 100%;
+        border-radius: 5px;
+    }
+</style>
---
+++ frontendDev/src/components/Player.vue
@@ -0,0 +1,264 @@
+<template>
+    <div class="playerContainer" @click="closePlayer()">
+        <div class="player">
+            <div class="left">
+                <div class="media">
+                    <iframe id="mediaFrame" :src="player.source" allowfullscreen frameborder="0" scrolling="no" sandbox="allow-scripts allow-forms allow-same-origin"></iframe>
+                </div>
+                <div class="info" v-if="data.show.info != undefined">
+                    <h1 class="showTitle">{{data.show.info.title}}</h1>
+                    <div class="year">
+                        <p>{{data.show.info.fromYear}}</p><p v-if="data.show.info.toYear != 0">- {{data.show.info.toYear}}</p>
+                    </div>
+                    <div class="genres" >
+                        <div class="genreTag" v-for="tag in data.show.info.genres" :key="tag">
+                            <p>{{tag}}</p>
+                        </div>
+                    </div>
+                    <p class="description">{{data.show.info.desc}}</p>
+                    <div class="actors" v-if="data.show.info.actors[0] != `undefined`">
+                        <h3>Schauspieler:</h3>
+                        <div class="infoTag" v-for="tag in data.show.info.actors" :key="tag">
+                            <p>{{tag}}</p>
+                        </div>
+                    </div>
+                    <div class="directors" v-if="data.show.info.directors[0] != `undefined`">
+                        <h3>Regisseure:</h3>
+                        <div class="infoTag" v-for="tag in data.show.info.directors" :key="tag">
+                            <p>{{tag}}</p>
+                        </div>
+                    </div>
+                    <div class="producers" v-if="data.show.info.producers[0] != `undefined`">
+                        <h3>Produzenten:</h3>
+                        <div class="infoTag" v-for="tag in data.show.info.producers" :key="tag">
+                            <p>{{tag}}</p>
+                        </div>
+                    </div>
+                    <div class="authors" v-if="data.show.info.authors[0] != `undefined`">
+                        <h3>Autoren:</h3>
+                        <div class="infoTag" v-for="tag in data.show.info.authors" :key="tag">
+                            <p>{{tag}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="episodes">
+                <div class="selectSeason">
+                    <select>
+                        <option v-for="season in data.show.seasons" :key="season">Staffel {{parseInt(season.index) + 1}}</option>
+                    </select>
+                </div>
+                <div class="seasons">
+                    <div class="season" v-for="season in data.show.seasons" :key="season">
+                        <h2>Staffel {{parseInt(season.index) + 1}}</h2>
+                        <div class="episode" v-for="episode in season.episodes" :key="episode" @click="setMediaSource(episode.vivo_link)">
+                            <p>{{episode.title}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { reactive, watch } from 'vue'
+export default {
+    name: "Player",
+    props:{
+        showID: Object,
+    },
+    methods: {
+
+    },
+    setup(props, context) {
+        const data = reactive({show: {}})
+        const seasonNr = reactive({value: 0})
+        const player = reactive({source: ""})
+
+        const host = "http://bstoapp.staging.it-tf.ch/api/"
+
+        watch(() => props.showID.value, async () => {
+            const response = await fetch(host + `episodes/${props.showID.value}`)
+            const show = await response.json()
+
+            let playerActive = false
+
+            for (let season in show.seasons) {
+                show.seasons[season].index = season
+
+                for (let episode in show.seasons[season].episodes) {
+                    show.seasons[season].episodes[episode].index = episode
+
+                    if (playerActive == false) { //hier watched check
+                        playerActive = true
+
+                        const vivoCode = show.seasons[season].episodes[episode].vivo_link.split("https://vivo.sx/")[1]
+                        
+                        player.source = "https://vivo.sx/embed/" + vivoCode
+                    }
+                }
+            }
+
+            data.show = show
+            console.log(data);
+
+            seasonNr.value = 0
+
+            const iframe = document.getElementById("mediaFrame")
+            iframe.addEventListener("load", () => {
+                const width = event.target.offsetWidth
+
+                event.target.style.height = width * 0.56 + "px"
+
+                const infos = document.getElementsByClassName("info")[0]
+                console.log(infos);
+
+                infos.style.height = document.getElementsByClassName("player")[0].offsetHeight - width * 0.56 -60 + "px"
+            })
+
+            document.getElementsByClassName("playerContainer")[0].style.display = "block"
+        })
+
+        function setMediaSource(url) {
+            const vivoCode = url.split("https://vivo.sx/")[1]
+            player.source = "https://vivo.sx/embed/" + vivoCode
+        }
+
+        function closePlayer() {
+            if (event.target.className == "playerContainer") {
+                document.getElementsByClassName("playerContainer")[0].style.display = "none"
+                player.source = ""
+            }
+        }
+
+        return {data, seasonNr, player, setMediaSource, closePlayer}
+    },
+    mounted(){
+
+    }
+}
+</script>
+
+<style scoped>
+    .playerContainer {
+        width: 100%;
+        height: 100vh;
+        position: fixed;
+        top: 0;
+        left: 0;
+        background-color: rgba(0, 0, 0, 0.616);
+        z-index: 100;
+        text-align: center;
+        display: none;
+    }
+
+    .player {
+        padding: 30px;
+        background-color: #394867;
+        display: inline-block;
+        border-radius: 5px;
+        margin-left: 5%;
+        margin-right: 5%;
+        margin-top: 5vh;
+        margin-bottom: 5vh;
+        height: calc(100vh - 10vh);
+        color: white;
+        text-align: left;
+        border-radius: 5px;
+    }
+
+    .media {
+        width: 100%;
+        vertical-align: top;
+        border-radius: 5px;
+    }
+
+    iframe {
+        width: 100%;
+        border-radius: 5px;
+    }
+
+    .left {
+        width: 50%;
+        display: inline-block;
+        vertical-align: top;
+    }
+
+    .info {
+        overflow-y: scroll;
+        vertical-align: top;
+    }
+
+    .showTitle {
+        font-size: 30pt;
+    }
+
+    .year {
+        margin-top: 5px;
+        margin-bottom: 5px;
+        font-size: large;
+    }
+
+    .genres {
+        margin-bottom: 10px;
+        margin-top: 5px;
+    }
+
+    p {
+        display: inline;
+    }
+
+    .episodes {
+        display: inline-block;
+        width: 50%;
+        height: 100%;
+        vertical-align: top;
+        overflow-y: scroll;
+    }
+
+    .season h2{
+        padding: 10px;
+        padding-top: 40px;
+    }
+
+    .episode {
+        width: calc(100% - 30px);
+        padding: 10px;
+        margin: 5px;
+        font-size: large;
+        box-shadow: 0px 0px 2px #1e2738;
+        cursor: pointer;
+    }
+
+    .episode:hover {
+        background-color:  #1e273856;
+    }
+
+    .genres {
+        margin-top: 20px;
+        margin-bottom: 20px;
+    }
+
+    .genreTag{
+        background-color: white;
+        border-radius: 18px;
+        color: #394867;
+        display: inline-block;
+        margin: 3px;
+        font-weight: bold;
+        padding: 4px;
+        padding-left: 8px;
+        padding-right: 8px;
+    }
+
+    h3 {
+        display: inline-block;
+    }
+
+    .infoTag {
+        display: inline-block;
+        margin: 3px;
+        margin-top: 6px;
+    }
+</style>
